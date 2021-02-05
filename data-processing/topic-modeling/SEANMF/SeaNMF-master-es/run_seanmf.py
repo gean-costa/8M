beab5020d67f61cca5e7c3ae8261ee8aa2bd5b05
Gean Costa <geandreson.costa@gmail.com>
2021-02-05 13:42:54 -0300

    Adicao da etapa de processamento: analise de sentimento, modelagem de topicos e outros.

---
+++ data-processing/topic-modeling/SEANMF/SeaNMF-master-es/run_seanmf.py
@@ -0,0 +1,10 @@
+import os
+from tqdm import trange
+
+os.system("python data_process.py")
+
+for i in trange(5,55,5):
+    command = f"python train.py --n_topics {i} > output{i}t.txt && python vis_topic.py >> output{i}t.txt"
+    print(command)
+    os.system(command=command)
+    
